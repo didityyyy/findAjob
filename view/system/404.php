58c668913e25c61593db76f51feeea5b42c49c2d
Dayana <didityyyy@abv.bg>
2023-06-20 16:55:46 +0300

    added missing files

---
+++ view/system/404.php
@@ -0,0 +1,9 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/DR/view/layout/header.php"; ?>
+
+<div class="login-bg height-bg flex-x">
+    <div class="login flex-y" style="text-align: center;">
+        <h2>Страницата не е открита!</h2>
+    </div>
+</div>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/DR/view/layout/footer.php"; ?>
